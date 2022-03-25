a10c0ce30c0b65ee3b5ab6f0b55753c090286916
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-25 20:46:06 +0300

    image class

--- gallery/models.py
+++ gallery/models.py
@@ -7,3 +7,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='artciles',blank =False)
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    author = models.CharField(max_length=70, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  
