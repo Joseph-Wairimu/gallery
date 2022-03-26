88e348254d5c87e4e0b1f05facbe67dba24858a9
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-26 10:30:11 +0300

    save and delete category function

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
-from .models import Category,Image, Location
+from .models import Category,Image
 
 admin.site.register(Category)
 admin.site.register(Image)
-admin.site.register(Location)
--- gallery/models.py
+++ gallery/models.py
@@ -3,29 +3,28 @@ from django.db import models
 # Create your models here.
 
 
-class Location(models.Model):
-    name = models.CharField(max_length=80 ,unique=True)  
-
-    
-    def __str__(self):
-        return self.name
-
 class Category(models.Model):
     name = models.CharField(max_length=80 , unique=True)
 
 
     def __str__(self):
         return self.name
+        
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to ='artciles',blank =False)
+    image = models.ImageField(upload_to ='gallery',blank =False)
     name = models.CharField(max_length=80)
     description = models.TextField()
     author = models.CharField(max_length=70, default='admin')
     date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+ 
   
 
     
