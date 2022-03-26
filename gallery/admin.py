418e77b1724d8d160ffe440011944a982d6eb547
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-26 09:56:48 +0300

    register location to admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from .models import Category,Image
+from .models import Category,Image, Location
 
 admin.site.register(Category)
 admin.site.register(Image)
+admin.site.register(Location)
