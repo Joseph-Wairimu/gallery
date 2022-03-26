893c96699263d9517fb2c4a8fcff0d475fc18fa9
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-26 12:45:59 +0300

    view function for searched articles

--- gallery/views.py
+++ gallery/views.py
@@ -7,3 +7,15 @@ def welcome(request):
 
 
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_articles = Image.search_by_category( category)
+        message = f"{ category}"
+
+        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
